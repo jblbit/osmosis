e5c00dc0976f78d99a4bb83c9252ab4cdfbf1b64
Roman <ackhtariev@gmail.com>
2022-04-12 20:20:17 -0400

    better name for interface extension

--- x/gamm/types/pool.go
+++ x/gamm/types/pool.go
@@ -74,7 +74,7 @@ type PoolI interface {
 	PokePool(blockTime time.Time)
 }
 
-type PoolExternExitSwapExternAmountOutExtension interface {
+type PoolExitSwapExternAmountOutExtension interface {
 	PoolI
 	ExitSwapExternAmountOut(
 		ctx sdk.Context,
